781efc139edbf7aeeb66c4dfcd579df19cb758c0
ursula Paul <ursulapaul31@gmail.com>
2025-01-02 14:50:55 +0100

    added barchat to my excel file

--- PycharmProjects - Copy/justpy/excel.py
+++ PycharmProjects - Copy/justpy/excel.py
@@ -9,8 +9,18 @@ print(sheet.max_row)
 
 for row in range(1, sheet.max_row + 1):
     cell = sheet.cell(row, 3)
-    corrected_price = cell.value * 0.9
+    corrected_price = (cell.value * 0.9)
     corrected_price_cell = sheet.cell(row, 4)
     corrected_price_cell.value = corrected_price
+
+values = Reference (sheet,
+           min_row=2,
+           max_row=sheet.max_row,
+           min_col=4,
+           max_col=4)
+
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
 
 wb.save('transactions2.xlsx')
